ce657762c25da70d6c98466908ba7ccd21a4a78d
swathiEulogik <swathi.n@eulogik.com>
2013-12-31 17:12:08 +0530

    deleting all junk files

--- calendar/src/dbConfig.php
+++
@@ -1,7 +0,0 @@
-<?
-	define("EZSQL_DB_USER", "root");			// <-- mysql db user
-	define("EZSQL_DB_PASSWORD", "");		// <-- mysql db password
-	define("EZSQL_DB_NAME", "skedulus");		// <-- mysql db pname
-	define("EZSQL_DB_HOST", "localhost");	// <-- mysql server host
-		 
-?>
